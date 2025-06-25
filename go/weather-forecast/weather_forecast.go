b1b118075b6b72d1727696a3b87d144722af6c26
Rovshan Muradov <63051606+RovshanMuradov@users.noreply.github.com>
2025-06-25 06:30:19 +0400

    completed task in  weather forecast

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather provides tool to predict current weather condidion.
 package weather
 
+// CurrentCondition declaration.
 var CurrentCondition string
+
+// CurrentLocation declaration.
 var CurrentLocation string
 
+// Forecast takes 2 parametrs and returns current weather condition.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
